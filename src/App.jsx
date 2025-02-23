4324cbfdf207627b12a58fc132777d12c35d0adf
Ayushi Verma <ayushiverma844@gmail.com>
2025-02-23 11:59:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './Components/Card';  
+
+function App() {
+  return (
+    <div className="app-container">
+      <Card />  {/* Render the Card component here */}
+    </div>
+  );
+}
+
+export default App;
